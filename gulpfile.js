89a15ee1b48b9515571f3dd317427c24b9de1345
Grey Grissom <grey.grissom@gmail.com>
2017-01-07 15:53:55 -0800

    Cleanup

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+watch      = require('gulp-watch'),
+    concat     = require('gulp-concat'),
+    mCSS       = require('gulp-minify-css'),
+
+   notify     = require('gulp-notify'),
+    uglify     = require('gulp-uglify'),
+    order      = require('gulp-order'),
+    clean      = require('gulp-cleanhtml'),
+    mario      = require('gulp-plumber');
